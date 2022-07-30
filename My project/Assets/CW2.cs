3c7bb25ecc9a3da30c75ba6bc2d22b734d6b96e8
Hassan Sh. AlShammari <100683964+Al3boodd@users.noreply.github.com>
2022-07-30 15:23:25 +0300

    99% done

---
+++ My project/Assets/CW2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CW2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //hero
+
+        string heroName = "SpiderMan";
+        int heroHeight = 169;
+        int heroAge = 18;
+        string heroSuperPower = "strings..";
+
+        //villain
+
+        string villainName = "Venom";
+        int vililanHeight = 229;
+        int villainAge = 13;
+        string villainSuperPower = "strong body..";
+        //Difference
+        int ageDifference = heroAge - villainAge
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        print("my hero is" + heroName + "the hero's height is" + heroHeight + "the hero's age is" + heroAge + "and my hero's super power is" + heroSuperPower);
+        print("my villain is" + villainName + "the villain height is" + vililanHeight + "the villain's age is" + villainAge + "and my villain's super power is" + villainSuperPower);
+    }
+}
+//اتمنى اعرف ليش مو قادر اسوي برنت :)
---
+++ cw2/Class1.cs
@@ -0,0 +1,29 @@
+﻿public class Class1
+{
+    public Class1()
+    {
+    }
+}
+
+string heroName = "SpiderMan";
+
+float heroHeight = 168.9;
+
+int heroAge = 18;
+
+string heroSuperPower = "strings..";
+
+string villainName = "Venom";
+
+float vililanHeight = 229;
+
+int villainAge = 13;
+
+string villainSuperPower = "strong body..";
+
+string heroName = "SpiderMan";
+
+int ageDifference = heroAge - villainAge;
+
+print("my hero is" + heroName + "the hero's height is" + heroHeight + "the hero's age is" + heroAge + "and my hero's super power is" + heroSuperPower);
+print("my villain is" + villainName + "the villain height is" + vililanHeight + "the villain age is" + villainAge "and my villain's super power is" + villainSuperPower);
